a68e1c975de5b032a6c6be929f710b5f66acefee
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-14 20:21:02 +0000

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ImageList from "./ImageList";
+import SearchBar from "./SearchBar";
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+      <ImageList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ImageList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ListDisplay = () => {
+  return (
+    <ul>
+      <li>Found Picture</li>
+      <li>Found Picture</li>
+      <li>Found Picture</li>
+    </ul>
+  );
+};
+
+export default ListDisplay;
