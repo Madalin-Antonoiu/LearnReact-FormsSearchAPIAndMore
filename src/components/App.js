627f95a83ebb0aea73a99a1b5389e3834906b43b
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-16 09:56:37 +0000

    .then vs async await

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import axios from "axios";
+
 import ImageList from "./ImageList";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    // Base URL of API + endpoint '/searc/photos'
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID ggHhOT9rdI3Zxj3FhCOlIR6jzXAw4Jl7FzwzOJGBeN0"
+      }
+    });
+    // .then((response) => {
+    //   console.log(response.data.results);
+    // });
+
+    console.log(response.data.results);
   }
 
   render() {
