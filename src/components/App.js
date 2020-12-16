02430ecc435ae5ce46a8746eb6c2f7a2e2df8674
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-16 12:09:38 +0000

    Data fetching in React !( before revamp)

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,28 @@
 import React from "react";
 import axios from "axios";
-
-import ImageList from "./ImageList";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  async onSearchSubmit(term) {
-    // Base URL of API + endpoint '/searc/photos'
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
     const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID ggHhOT9rdI3Zxj3FhCOlIR6jzXAw4Jl7FzwzOJGBeN0"
       }
     });
-    // .then((response) => {
-    //   console.log(response.data.results);
-    // });
 
-    console.log(response.data.results);
-  }
+    //console.log(response.data.results);
+    // console.log(this);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList />
+        Found: {this.state.images.length} images
       </div>
     );
   }
