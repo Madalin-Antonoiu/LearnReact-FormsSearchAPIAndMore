a272a862a14dab54a37037ba8c223637cd5b56cc
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-16 16:59:35 +0000

    React refs for images and callback event listeners

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.callback);
+  }
+
+  callback = () => {
+    console.log(this.imageRef.current.clientHeight);
+  };
+
   render() {
+    const { description, urls } = this.props.image; // Destructuring
+
     return (
       <div>
-        <img
-          alt={this.props.image.description}
-          src={this.props.image.urls.regular}
-        />
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
