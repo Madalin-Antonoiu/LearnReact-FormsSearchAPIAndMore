8f2441b02b0aa6a7c3f67cec76c8f1ad6199e180
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-12-16 16:44:02 +0000

    with ImageCard

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,18 +1,13 @@
 import "./ImageList.css";
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ListDisplay = (props) => {
-  //console.log(props.images);
-  const images = props.images.map(({ description, id, urls }) => (
-    <img alt={description} key={id} src={urls.thumb} /> //regular, small, thumb, full, raw
-  ));
+  const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />; //regular, small, thumb, full, raw
+  });
 
   return <div className="image-list">{images}</div>;
-
-  // Works just fine too:
-  // return props.images.map(({ description, id, urls }) => (
-  //   <img alt={description} key={id} src={urls.thumb} /> //regular, small, thumb, full, raw
-  // ));
 };
 
 export default ListDisplay;
